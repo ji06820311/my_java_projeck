4e99b01ea6bad5de30f893d5d20bd116d6f747ec
pro <jewangkka014@gmail.com>
2025-07-29 13:02:33 +0900

    7월 29일 세번 째2

--- src/ch02_control_statement/WhileTest4.java
+++ src/ch02_control_statement/WhileTest4.java
@@ -8,17 +8,17 @@ public class WhileTest4 {
         int total = 0;  // 합계
         int count = 0;  // 횟수
 
-        while (true){
+        while (true) {     // while 무한 루프
             System.out.println("점수 입력 : ");
             int grade = scan.nextInt();
 
-            if(grade < -10){
+            if (grade < -10) { //숫자가 -10 미만인 경우에 대한 처리
                 System.out.println("프로그램을 종료합니다.");
                 break;
 
-            }else if(grade <0) {
+            } else if (grade < 0) {  // -10 이상의 음수에 대한 처리
                 grade = -grade;
-            }else {
+            } else {
 
             }
             total += grade;
