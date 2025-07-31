7b56d44974ec63b3e7cc7bd0ace24d13646a055f
pro <jewangkka014@gmail.com>
2025-07-31 14:56:05 +0900

    7월 31일 이제 두번째 레슨2

---
+++ src/ch04_class/Movie03.java
@@ -0,0 +1,44 @@
+package ch04_class;
+
+public class Movie03 {
+    private String name;
+    private String director;
+    private int time;
+    private double money;
+
+    //setName(쓰기)
+    public void setName(String name) {
+        this.name = name;
+    }
+    //getName(읽기)
+    public String getName() {
+        return name;
+    }
+    // setTime(쓰기)
+    public void setTime(int time) {
+        this.time = time;
+    }
+    // getTime(읽기)
+    public int getTime() {
+        return time;
+    }
+
+    // setDirector(쓰기)
+    public void setDirector(String director){
+        this.director = director;
+    }
+    //getDirector(일기)
+    public String getDirector(){
+        return director;
+    }
+    //setMoney(쓰기)
+    public void setMoney(double money){
+        this.money = money;
+    }
+    //getMoney(읽기)
+    public double getMoney(){
+        return  money;
+    }
+
+
+}
---
+++ src/ch04_class/MovieMain03.java
@@ -0,0 +1,18 @@
+package ch04_class;
+
+public class MovieMain03 {
+    public static void main(String[] args) {
+        Movie03 fatasti4 = new Movie03();
+        // 쓰기
+        fatasti4.setName("판타스틱4");
+        fatasti4.setDirector("matt Shakman");
+        fatasti4.setTime(114);
+        fatasti4.setMoney(13000.0);
+        // 읽기
+        System.out.println("영화 이름은 :"+ fatasti4.getName());
+        System.out.println("영화 감독은 :"+ fatasti4.getDirector());
+        System.out.println("영화 시간은 :"+ fatasti4.getTime()+"분");
+        System.out.println("영화표 가격은 :"+ fatasti4.getMoney());
+
+    }
+}
