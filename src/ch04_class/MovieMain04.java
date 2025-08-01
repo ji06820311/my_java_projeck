fc1cf316bc359ff45d8d716ba8e62076de08cd41
pro <jewangkka014@gmail.com>
2025-08-01 14:48:56 +0900

    8월 1일 두번째 레슨(3)

--- src/ch04_class/MovieMain04.java
+++ src/ch04_class/MovieMain04.java
@@ -4,8 +4,8 @@ public class MovieMain04 {
     public static void main(String[] args) {
         Movie04[] movie = new Movie04[1];   //대상 총합 개수        //2번째로 작성
 
+        // 위에는 대상 종합 개수면 이거는 그냥 각각 대상 개수를 적는다 예를들면 두개의 대상이 있으면 0(1) 1(2) 2(3) 이렇게 5개의 대상이 있으면 하나씩 줄여들어서 개수는 4개
         movie[0] = new Movie04("판사스틱4", "matt Shakman", 114, 13.000); //하나이면 0, 둘이면 2
-
 
         for (int i = 0; i < movie.length; i++) { //5번째 작성
             movie[i].display();
