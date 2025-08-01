01722e020d797d9bc52f4cc16f018dc164f7369e
pro <jewangkka014@gmail.com>
2025-08-01 14:34:44 +0900

    8월 1일 두번째 레슨(2)

--- src/ch04_class/Movie04.java
+++ src/ch04_class/Movie04.java
@@ -1,19 +1,19 @@
 package ch04_class;
 
 public class Movie04 {
-    private String name;
+    private String name;      //1번째로 작성
     private String director;
     private int time;
     private double money;
 
-    public Movie04(String name, String director, int time, double money) {
+    public Movie04(String name, String director, int time, double money) {   //3번째로 작성
         this.name = name;
         this.director = director;
         this.time = time;
         this.money = money;
     }
 
-    public void display() {
+    public void display() { // 4번째로 작성
         System.out.println("영화 이름은 :" + this.name);
         System.out.println("영화 감독은 :" + this.director);
         System.out.println("영화 시간은 :" + this.time);
--- src/ch04_class/MovieMain04.java
+++ src/ch04_class/MovieMain04.java
@@ -2,12 +2,12 @@ package ch04_class;
 
 public class MovieMain04 {
     public static void main(String[] args) {
-        Movie04[] movie = new Movie04[1];   //대상 총합 개수
+        Movie04[] movie = new Movie04[1];   //대상 총합 개수        //2번째로 작성
 
         movie[0] = new Movie04("판사스틱4", "matt Shakman", 114, 13.000); //하나이면 0, 둘이면 2
 
 
-        for (int i = 0; i < movie.length; i++) {
+        for (int i = 0; i < movie.length; i++) { //5번째 작성
             movie[i].display();
         }
 
